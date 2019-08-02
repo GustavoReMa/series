c9d1d41b44893cf09a1c695876689a665fbd0ca2
GustavoSH <greyes@mediomelon.mx>
2019-08-02 17:58:35 -0500

    Modificación entidad User

--- seriesApp/app/src/main/java/com/example/series/api/data/User.java
+++ seriesApp/app/src/main/java/com/example/series/api/data/User.java
@@ -2,16 +2,10 @@ package com.example.series.api.data;
 
 public class User {
 
-    private String name;
     private String token;
 
     public User(String name, String token) {
-        this.name = name;
         this.token = token;
-    }
-
-    public String getName() {
-        return name;
     }
 
     public String getToken() {
